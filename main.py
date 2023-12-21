f12ac8a0a3731cf663098dac298fd3358b26e295
Yi He <hyyy@umich.edu>
2023-12-21 16:00:11 +0800

    impl checkout

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import *
 import sqlite3
 import os
 from werkzeug.utils import secure_filename
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.secret_key = 'random string'
@@ -102,23 +104,90 @@ def addItem():
         conn.close()
         return render_template('addItem.html',categories=categories)
 
-@app.route('/cart',methods=['GET'])
+@app.route('/cart',methods=['GET','POST'])
 def cart():
     if 'email' not in session:
         return redirect(url_for('login'))
     loggedIn, name, noOfItems = getLoginDetails()
     email = session['email']
+
+    if request.method == "GET":
+        with sqlite3.connect('database.db') as conn:
+            cur = conn.cursor()
+            cur.execute("SELECT userId FROM Users WHERE email = ?", (email, ))
+            userId = cur.fetchone()[0]
+            cur.execute("SELECT products.productId, products.name, products.price, products.image, products.inventoryAmount, cart.inventoryAmount FROM products, cart WHERE products.productId = cart.productId AND cart.userId = ?", (userId, ))
+            products = cur.fetchall()
+        totalPrice = 0
+        for row in products:
+            totalPrice += row[2]
+
+        return render_template("cart.html", products = products, totalPrice=totalPrice, loggedIn=loggedIn, name = name, noOfItems=noOfItems)
+    else:
+        # checkout
+        shippingAddress = request.args.get('shippingAddress', '')  
+        with sqlite3.connect('database.db') as conn:
+            cur = conn.cursor()
+
+            cur.execute("SELECT userId FROM Users WHERE email = ?", (email,))
+            userId = cur.fetchone()[0]
+
+            
+            cur.execute('''SELECT p.productId, p.inventoryAmount
+                           FROM Products p
+                           JOIN cart k ON p.productId = k.productId
+                           WHERE k.userId = ?''', (userId,))
+            products = cur.fetchall()
+
+            cur.execute("SELECT productId, inventoryAmount FROM cart WHERE userId = ?", (userId,))
+            cart_items = cur.fetchall()
+            if not cart_items:
+                return render_template('checkout.html',  loggedIn=loggedIn, name=name, message="Nothing to checkout.")
+
+            for productId, quantity in cart_items:
+                cur.execute("SELECT inventoryAmount FROM Products WHERE productId = ?", (productId,))
+                inventoryAmount = cur.fetchone()[0]
+                if quantity > inventoryAmount:
+                    return render_template('checkout.html', loggedIn=loggedIn, name=name, message="Insufficient stock for one or more items.")
+
+            if any(inventoryAmount <= 0 for _, inventoryAmount in products):
+                return render_template('checkout.html', loggedIn=loggedIn, name=name, message="Error: One or more products are out of stock.")
+
+            # Process each product in the cart
+            for productId, _ in products:
+                # Insert a transaction record
+                cur.execute('''INSERT INTO Transactions (date, userId, productId, quantity, shippingAddress) 
+                               VALUES (?, ?, ?, ?, 1)''', (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), userId, productId, quantity, shippingAddress))
+                
+                # Reduce the inventoryAmount by 1
+                cur.execute("UPDATE Products SET inventoryAmount = inventoryAmount - 1 WHERE productId = ?", (productId,))
+
+            # Clear the user's cart
+            cur.execute("DELETE FROM cart WHERE userId = ?", (userId,))
+
+            conn.commit()
+
+        return render_template('checkout.html',  loggedIn=loggedIn, name=name, message="Transaction successfully made.")
+
+@app.route("/removeFromCart")
+def removeFromCart():
+    if 'email' not in session:
+        return redirect(url_for('login'))
+    email = session['email']
+    productId = int(request.args.get('productId'))
     with sqlite3.connect('database.db') as conn:
         cur = conn.cursor()
         cur.execute("SELECT userId FROM Users WHERE email = ?", (email, ))
         userId = cur.fetchone()[0]
-        cur.execute("SELECT products.productId, products.name, products.price, products.image, products.inventoryAmount, cart.inventoryAmount FROM products, cart WHERE products.productId = cart.productId AND cart.userId = ?", (userId, ))
-        products = cur.fetchall()
-    totalPrice = 0
-    for row in products:
-        totalPrice += row[2]
-
-    return render_template("cart.html", products = products, totalPrice=totalPrice, loggedIn=loggedIn, name = name, noOfItems=noOfItems)
+        try:
+            cur.execute("DELETE FROM cart WHERE userId = ? AND productId = ?", (userId, productId))
+            conn.commit()
+            msg = "removed successfully"
+        except:
+            conn.rollback()
+            msg = "error occured"
+    conn.close()
+    return redirect(url_for('cart'))
 
 @app.route('/account/orders',methods=['GET'])
 def orders():
